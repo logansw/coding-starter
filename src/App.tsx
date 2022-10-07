7b476a445835efffd4fe519145a60b95515054b2
logansw <logansw@uw.edu>
2022-10-07 15:13:18 -0700

    Done! Sorta

--- src/App.tsx
+++ src/App.tsx
@@ -3,35 +3,53 @@ import "css/global/Global.css";
 import { Route, Routes as RoutesImport, BrowserRouter } from "react-router-dom";
 import EXAMPLE_SUBMISSIONS, { Submission } from "ExampleSubmissions";
 import styles from "css/App.module.css";
+import getLocalStorage from './utils/local-storage/getLocalStorage';
+import setLocalStorage from './utils/local-storage/setLocalStorage';
+import { useState } from 'react';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const USER_ID = 0;
+let remainingSubmissions = EXAMPLE_SUBMISSIONS;
+
+// EXAMPLE_SUBMISSION.filter(submission => submission != )
 
 function VotePage(): JSX.Element {
-  const example: Submission = EXAMPLE_SUBMISSIONS[0];
-  console.log(example);
+  const [idx, setIdx] = useState(0);
+  const [curSubmission, setCurSubmission] = useState(remainingSubmissions[idx]);
+
+  function Vote(user: number, submission: number, up: boolean): void {
+    let votes: any = getLocalStorage<Object[]>("Votes");
+    if (votes == null) {
+      setLocalStorage("Votes", [{user: user, submission: submission, up: up}]);
+    } else {
+      console.log(votes);
+      votes.push({user: user, submission: submission, up: up});
+    }
+    setLocalStorage("Votes", votes);
+    setIdx(idx + 1);
+  }
 
   return (
     <div className={styles.container}>
-      <div className={styles.example}>
-        <h1>Welcome!</h1>
-        <ul>
-          <li>
-            You&apos;re currently looking at <strong>App.tsx</strong>
-          </li>
-          <li>
-            Local storage helpers have been defined for your convenience under{" "}
-            <strong>utils/local-storage</strong>
-          </li>
-          <li>Check your JS console for the data of an example submission.</li>
-        </ul>
-        <div>Good luck!</div>
+      <div className={styles.name}>
+        {curSubmission["name"]}
       </div>
+      <div className={styles.images}>
+        <img src={curSubmission["assets"][0]["src"]} />
+        <img src={curSubmission["assets"][1]["src"]} />
+        <img src={curSubmission["assets"][2]["src"]} />
+      </div>
+      <button onClick={() => Vote(USER_ID, idx, false)}>Downvote</button>
+      <button onClick={() => Vote(USER_ID, idx, true)}>Upvote</button>
     </div>
   );
 }
 
 function App() {
+  let votes: any = getLocalStorage<Object[]>("Votes");
+  votes = votes?.filter(vote => vote["user"] === USER_ID);
+  remainingSubmissions = EXAMPLE_SUBMISSIONS.filter(submission => votes?.find(vote => vote.submission == submission.id) === null);
+
   return (
     <BrowserRouter>
       <RoutesImport>
@@ -42,3 +60,12 @@ function App() {
 }
 
 export default App;
+
+/*
+
+Votes
+  User
+  Submission
+  Upvote/Downvote
+
+*/
